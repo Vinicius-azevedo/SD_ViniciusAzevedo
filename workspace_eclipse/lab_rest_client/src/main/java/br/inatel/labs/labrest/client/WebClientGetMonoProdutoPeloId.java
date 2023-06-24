5d68c1dfd94e25c5027c23e3e0fb916ce3921c6a
Vinicius-azevedo <viniciusdeoliveira16@hotmail.com>
2023-06-24 13:12:16 -0300

    Subindo projetos

---
+++ workspace_eclipse/lab_rest_client/src/main/java/br/inatel/labs/labrest/client/WebClientGetMonoProdutoPeloId.java
@@ -0,0 +1,36 @@
+package br.inatel.labs.labrest.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import br.inatel.labs.labrest.client.model.dto.ProdutoDTO;
+import reactor.core.publisher.Mono;
+
+public class WebClientGetMonoProdutoPeloId {
+
+public static void main(String[] args) {
+		
+		
+		try {
+			Mono<ProdutoDTO> monoProduto = WebClient.create("http://localhost:8080")
+					.get()
+					.uri("/produto/1")
+					.retrieve()
+					.bodyToMono(ProdutoDTO.class)
+					;
+			
+			monoProduto.subscribe();
+			
+			ProdutoDTO produtoEncotrado = monoProduto.block();
+			
+			System.out.println("Produto encontrado:");
+			System.out.println(produtoEncotrado);
+		} catch (WebClientResponseException e) {
+			System.out.println("Status da resposta: " + e.getStatusCode());
+			System.out.println("Response body: " + e.getResponseBodyAsString());
+		}
+		}
+}
---
+++ workspace_eclipse/lab_rest_client/src/test/java/br/inatel/labs/labrest/client/LabRestClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labrest.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabRestClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
